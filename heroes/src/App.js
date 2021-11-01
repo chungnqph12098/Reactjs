36c2b66136f0402a74fd6a7e4ef9dd453cd94b4c
chung200121 <chungnqph12098@fpt.edu.vn>
2021-11-02 02:59:18 +0700

    Ex1

---
+++ heroes/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Heros from './Exercise1/Heros';
+
+function App() {
+  return (
+    <div className="App">
+      <Heros/>
+    </div>
+  );
+}
+
+export default App;
